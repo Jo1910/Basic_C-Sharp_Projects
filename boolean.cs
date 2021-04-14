571292aeb5fd0826e4e38551062055c54da97386
Jo1910 <35170540+Jo1910@users.noreply.github.com>
2021-04-14 16:13:43 +0100

    Add Boolean Logic Assignment

---
+++ boolean.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI violation?(yes/no)");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets have you had?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool license = (age >= 15 && dui == "no" && tickets <= 3);
+            Console.WriteLine(license);
+            Console.ReadLine();
+
+        }
+    }
+}
