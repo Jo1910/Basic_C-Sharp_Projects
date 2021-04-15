e4d4df0147d17e7100c1ce0d7c3e029efe3238a5
Jo1910 <jovanamitic@ymail.com>
2021-04-15 18:27:33 +0100

    Add some changes to Loops assignment

--- LoopsAssignment/LoopsAssignment/LoopsAssignment/Program.cs
+++ LoopsAssignment/LoopsAssignment/LoopsAssignment/Program.cs
@@ -42,6 +42,54 @@ namespace LoopsAssignment
                 }
             }
             Console.ReadLine();
+
+
+            // using do while statement
+            Console.WriteLine("Guess the color");
+            string color = Console.ReadLine();
+            bool guessColor = color == "purple";
+
+            do
+            {
+                switch (color)
+                {
+                    case "red":
+                        Console.WriteLine("Red is not correct.");
+                        Console.WriteLine("Guess the color");
+                        color = Console.ReadLine();
+                        break;
+                    case "blue":
+                        Console.WriteLine("Blue is not correct.");
+                        Console.WriteLine("Guess the color");
+                        color = Console.ReadLine();
+                        break;
+                    case "green":
+                        Console.WriteLine("Green is not correct.");
+                        Console.WriteLine("Guess the color");
+                        color = Console.ReadLine();
+                        break;
+                    case "purple":
+                        Console.WriteLine("That's correct.");
+                        guessColor = true;
+                        break;
+                    case "yellow":
+                        Console.WriteLine("Yellow is not correct.");
+                        Console.WriteLine("Guess the color");
+                        color = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("That's the wrong answer.");
+                        Console.WriteLine("Guess the color");
+                        color = Console.ReadLine();
+                        break;
+                }
+
+            }
+            while (!guessColor);
+
+            Console.ReadLine();
         }
-    }
+    }  
 }
+   
+
