560bbcfa6606abe394bb4777681d13474ea4de9e
Jo1910 <jovanamitic@ymail.com>
2021-04-16 12:03:35 +0100

    Add String Assignment

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssign
+{
+    class Program
+    {
+        static void Main()
+        {
+            string firstName = "Sarah";
+            string lastName = "Dean";
+            string occupation = "admin";
+
+            // string concenation
+            string sentence = (firstName + " " + lastName + " is an " + occupation + " in this company.");
+            Console.WriteLine(sentence);
+            Console.ReadLine();
+
+            // converting to uppercase
+            sentence = sentence.ToUpper();
+            Console.WriteLine(sentence);
+            Console.ReadLine();
+
+            //stringBuilder
+
+            StringBuilder sb = new StringBuilder("Once upon a time there lived for cows in the forest.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+            sb.Append(" Every day they used to graze together in a particular spot.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+            sb.Append(" They were all friends.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+            sb.Append(" One day a lion saw the cows grazing together.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
