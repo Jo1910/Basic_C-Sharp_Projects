823a71ca8b169be6080104700524fcb7f5f1b15f
Jo1910 <jovanamitic@ymail.com>
2021-04-27 16:04:11 +0100

    Add Method Assignment Folder

---
+++ Method Assignment/MethodAssignment/MethodAssignment/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Math
+    {
+        public int addNumbers(int userNum)
+        {
+            return userNum + 156;
+        }
+
+        public int multNumbers(int userNum)
+        {
+            return userNum * 233;
+        }
+
+        public int subNumbers(int userNum)
+        {
+            return userNum - 8;
+        }
+    }
+}
---
+++ Method Assignment/MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+
+            Console.WriteLine("\n" + userNum + " + 156 " + " equals " + math.addNumbers(userNum));
+            Console.WriteLine("\n" + userNum + " * 233 " + " equals " + math.multNumbers(userNum));
+            Console.WriteLine("\n" + userNum + " - 8 " + " equals " + math.subNumbers(userNum));
+
+            Console.ReadLine();
+        }
+        
+    }
+    
+}
