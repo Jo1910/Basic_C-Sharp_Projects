a0ef6c2e98a6db5fb254d4d01e945e6146ec33fe
Jo1910 <jovanamitic@ymail.com>
2021-04-27 18:05:59 +0100

    Add Method Assignment 1 folder

---
+++ Method Assignment 1/MethodAssign1/MethodAssign1/MathOper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign1
+{
+    class MathOper //creating class "MathOper"
+    {
+        public int mathOperation(int num1)  // creating a method that will take in integer as a parameter
+        {
+            return num1 + 13;
+        }
+
+        public int mathOperation(decimal num2) // creating a method with the same name, different parameter
+        {
+            return Convert.ToInt32(num2) + 20;
+        }
+        
+        public int mathOperation(string num3) // creating a method with the same name, different parameter
+        {
+            return Convert.ToInt32(num3) * 40;
+        }
+    }
+}
---
+++ Method Assignment 1/MethodAssign1/MethodAssign1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MethodAssign1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOper math = new MathOper(); // instantiating the class
+
+            // Displaying the results to the screen
+            Console.WriteLine("\n\t 13 + 13 equals " + math.mathOperation(13) + ".");
+            Console.WriteLine("\n\t 20 + 3.4, rounded to the nearest integer, equals " + math.mathOperation(3.4m) + ".");
+            Console.WriteLine("\n\t 20 * 50 equals " + math.mathOperation("50") + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
