417329212e9d3c7a20447eee5f8e538d9f809ab7
Jo1910 <jovanamitic@ymail.com>
2021-04-28 18:05:13 +0100

    Add Class Method Assignment Folder

---
+++ Class Method Assignment/ClassMethodAssign/ClassMethodAssign/MathOp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssign
+{
+    static class MathOp
+    {
+        public static void divideNum(int userNum)
+        {
+            int result = userNum / 2;
+            Console.WriteLine("\n\t" + userNum + " divided by 2 equals " + result);
+        }
+
+        public static void addNum(out int x, out int y) // creating a method with output parameters
+        {
+            x = 15;
+            y = 20;
+
+            Console.WriteLine("\n\t" + x + " + " + y + " equals " + (x + y));
+        }
+
+        public static void addNum(out double a, out double b)      // overloading a method
+        {
+            a = 4.6;
+            b = 5.8;
+
+            Console.WriteLine("\n\t" + a + " + " + b + " equals " + (a + b));
+        }
+    }
+}
---
+++ Class Method Assignment/ClassMethodAssign/ClassMethodAssign/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassMethodAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:\n");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+
+            //MathOp math = new MathOp(); // instatiating the class "MathOp" (commented out because I declared 
+                                          // the class "MathOp" to be static
+            MathOp.divideNum(userNum);  // calling the method on the number entered by user
+            Console.ReadLine(); // displaying the outpu to the screen
+
+            // calling a method with output parameters
+            int x;
+            int y;
+            MathOp.addNum(out x, out y);
+            Console.ReadLine();
+
+            // calling a overloaded method
+            double a;
+            double b;
+            MathOp.addNum(out a, out b);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
