d05ff948c4b07defa83a2a47364ed6f6931398bf
Jo1910 <jovanamitic@ymail.com>
2021-04-28 11:49:23 +0100

    Add Method Assignment 3 Folder

---
+++ Method Assignment 3/MethodAssign3/MethodAssign3/MathOper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MethodAssign3
+{
+    class MathOper
+    {
+        public void Math(int num1, int num2)
+
+        { 
+            int result = num1 + 15;
+            Console.WriteLine("The second integer is " + num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Method Assignment 3/MethodAssign3/MethodAssign3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOper showNum = new MathOper();
+            showNum.Math(4, 5);
+            showNum.Math(num1: 4, num2: 3);
+        }
+    }
+}
