d7c13fa0fc5ded9d9ea7a6f563663574db5e5d2b
Jo1910 <jovanamitic@ymail.com>
2021-04-28 17:00:08 +0100

    Add Method Assignment 2 Folder

---
+++ Method Assignment 2/MethodAssign2/MethodAssign2/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign2
+{
+    class Math
+    {
+        public int mathOp(int userNum, int optNum = 6)
+        {
+            return ((userNum + optNum) * 10);
+        }
+    }
+}
---
+++ Method Assignment 2/MethodAssign2/MethodAssign2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("\nPlease enter a number.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nPlease enter a second number, if you wish (it is optional).");
+            string b = Console.ReadLine();
+            bool inputFound = string.IsNullOrEmpty(b);
+            
+
+            if (inputFound == false) {
+                int x = Convert.ToInt32(b);
+                Console.WriteLine("\n\tThe sum of " + a + " and " + x + ", multiplied by 10, equals " + math.mathOp(a, x) + ".");
+            }
+            else
+            {
+                Console.WriteLine("\n\tThe sum of " + a + " and " + "our optional number (6)" + ", multiplied by 10, equals " + math.mathOp(a) + ".");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
