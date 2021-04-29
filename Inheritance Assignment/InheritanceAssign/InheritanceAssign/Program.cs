b80b7576677a8fad19af0d3c689a9f6c1f793972
Jo1910 <jovanamitic@ymail.com>
2021-04-29 11:52:08 +0100

    Add inheritance assignment folder

---
+++ Inheritance Assignment/InheritanceAssign/InheritanceAssign/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InheritanceAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.FirstName = "Sample";
+            emp.LastName = "Student";
+            emp.SayName();
+            Console.ReadLine();
+        }
+    }
+}
