1fcfe41f1c685d2052e95690844f7d21f157eea6
Jo1910 <jovanamitic@ymail.com>
2021-05-03 16:16:08 +0100

    Add generic assignment folder

---
+++ Generic Assignment/GenericsAssign/GenericsAssign/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsAssign
+{
+    public class Employee<T> // creating the "Employee" class that takes generic type parameter
+    {
+        public List<T> things { get; set; } //adding a property called "things" with generic list as a data type
+
+    }
+}
---
+++ Generic Assignment/GenericsAssign/GenericsAssign/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GenericsAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>(); // instatiating an Employee object with string type as its generic parameter
+            employee.things = new List<string>() { "Dina", "Alen", "Maria", "Peter" }; // assigning a list of strings as the property value of things
+
+            Employee<int> employeeAge = new Employee<int>(); // instatiating an Employee object with integer type as its generic parameter
+            employeeAge.things = new List<int> { 22, 50, 33, 44 }; // assigning a list of integers as the property value of things
+
+            //foreach (string emp in employee.things) // creating a loop that prints employees names
+            //{
+            //    Console.Write(emp + " " + '\n');
+            //}
+            //foreach (int age in employeeAge.things) // creating a loop that prints employees age
+            //{
+            //    Console.Write(age + " " +'\n');
+            //}
+            //Console.ReadLine();
+
+            int i = 0;
+            foreach (string emp in employee.things)  // loop that prints both lists
+            {
+                Console.WriteLine(emp + " " + employeeAge.things[i++]);
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
