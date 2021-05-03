6bab8aae6d9bb2810cc7ad7f632c46475f89360d
Jo1910 <jovanamitic@ymail.com>
2021-05-03 10:57:48 +0100

    Add operator overload assignment

---
+++ Overload Operator/OverloadOper/OverloadOper/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOper
+{
+    public class Employee // creating a class called "Employee"
+    { 
+    public int Id { get; set; } // giving it a property "Id" of integer type
+
+
+    //// comparing two objects using comparison operator overloading
+    //// commented out because it's less code
+    
+    //public static bool operator == (Employee employee1, Employee employee2)
+    //{
+    //    if (employee1.Id == employee2.Id)
+    //    {
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+    //}
+
+    //public static bool operator != (Employee employee1, Employee employee2)
+    //{
+    //    if (employee1.Id != employee2.Id)
+    //    {
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+    //}
+
+   
+    public override bool Equals(object obj) // overriding the equals method in order to test for value equality
+     {
+        if (obj == null) // checking that argument is not null
+        {
+           return false;
+        }
+        if (!(obj is Employee)) // if the passed in object is not "Employee" type or can't be referenced to it, return false
+        {
+           return false;
+        }
+           return this.Id == ((Employee)obj).Id; // checking if passed in object Id is equal to this instance of Id
+        }
+        public override int GetHashCode() // overriding the hash method to allow a type to work correctly in a hash table
+        {
+          return this.Id.GetHashCode();
+     }
+
+    }   
+}
---
+++ Overload Operator/OverloadOper/OverloadOper/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OverloadOper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee(); // creating an instance of "Employee" class
+            employee1.Id = 11;
+
+            Employee employee2 = new Employee();
+            employee2.Id = 13;
+
+
+            Console.WriteLine(employee1.Equals(employee2)); // method equals checking the value equality between
+                                                                // two objects
+            Console.ReadLine();
+            
+
+            //// comparing and displaying using comparison operator
+
+            //if (employee1.Id == employee2.Id)
+            //{
+            //    Console.WriteLine("Employees IDs are the same.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Employees IDs are not the same");
+            //}
+            //Console.ReadLine();
+
+        }
+
+    }
+}
