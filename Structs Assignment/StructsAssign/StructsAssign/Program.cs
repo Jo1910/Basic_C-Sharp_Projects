a931f828cc2b1e1ce90ba7914b7766ee1713f4b4
Jo1910 <jovanamitic@ymail.com>
2021-05-04 15:49:00 +0100

    Add Structs assignment folder

---
+++ Structs Assignment/StructsAssign/StructsAssign/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssign
+{
+    class Program
+    {
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Number n = new Number();
+            n.Amount = 134.567m;
+            Console.WriteLine("The amount is " + n.Amount);
+            Console.ReadLine();
+        }
+    }
+}
