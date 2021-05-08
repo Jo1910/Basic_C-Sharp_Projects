380c4770a5b36114e118828a4dabf101de3a4576
Jo1910 <jovanamitic@ymail.com>
2021-05-08 12:05:59 +0100

    Add File I\O assignment

---
+++ File IO Assignment/IOassign/IOassign/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOassign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\mitic\Documents\io.txt", number);
+            Console.WriteLine("The number you chose is " + File.ReadAllText(@"C:\Users\mitic\Documents\io.txt") + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
