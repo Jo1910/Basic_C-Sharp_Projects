f5aa89f92615c03c7c56c3c8cba94bee5c1f25dc
Jo1910 <jovanamitic@ymail.com>
2021-05-08 14:37:57 +0100

    Add datetime assignment folder

---
+++ DateTime Assignment/DateTimeAssign/DateTimeAssign/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeNow = DateTime.Now;
+            Console.WriteLine("The current date and time is: " + timeNow);
+            Console.WriteLine("Please enter a number:");
+            int X = Convert.ToInt32(Console.ReadLine());
+            DateTime timeFuture = timeNow.AddHours(X);
+            Console.WriteLine("In " + X+ " hours will be " + timeFuture + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
