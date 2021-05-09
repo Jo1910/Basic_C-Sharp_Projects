f1b1c9943537d3e8c5f2cb4a08bc8fe462bb7cf3
Jo1910 <jovanamitic@ymail.com>
2021-05-09 11:51:36 +0100

    Add additional features assignment

---
+++ Additional Features/AddFeatures/AddFeatures/DunderMifflinEmployees.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace AddFeatures
+{
+    class DunderMifflinEmployees
+    {
+        string firstName;
+        string lastName;
+        string occupation;
+
+        public DunderMifflinEmployees() : this("Michael", "Scott", "the regional manager")
+        {
+
+        }
+
+        public DunderMifflinEmployees(string firstName, string lastName, string occupation)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.occupation = occupation;
+        }
+
+        public void employeeIs()
+        {
+            Console.WriteLine(firstName + " " + lastName + " is " + occupation + ".");
+        }
+   
+    
+
+    }
+}
---
+++ Additional Features/AddFeatures/AddFeatures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace AddFeatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string company = "Dunder Mifflin Paper Co.";
+            Console.WriteLine("Welcome to {0}", company);
+          
+
+            DunderMifflinEmployees employee = new DunderMifflinEmployees();
+            employee.employeeIs();
+            Console.ReadLine();
+        }
+    }
+}
