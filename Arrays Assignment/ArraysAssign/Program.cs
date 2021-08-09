c1ba454c8169a7dcbbb80694aee553609ca94860
Jo1910 <jovanamitic@ymail.com>
2021-08-09 17:47:48 +0100

    Refactoring an array assignment

--- Arrays Assignment/ArraysAssign/Program.cs
+++ Arrays Assignment/ArraysAssign/Program.cs
@@ -8,91 +8,56 @@ namespace ArraysAssign
     {
         static void Main()
         {
+            // This assignment calls for creating a one-dimensional array of strings, array of integers
+            // and a list of strings, asking for a user input and displaying the value of that index.
+
+
+
             // Array of strings
-            string[] myStrArray = { "Have", "a", "nice", "day" };
-            Console.WriteLine("Please select an index of an array: ");
-            int userIndex = Convert.ToInt32(Console.ReadLine());
+            string[] colors = { "red", "blue", "green", "yellow" }; //creating an array of strings
+            Console.WriteLine("Please select a number between 0 and 3 to choose a color: "); // asking for user input
+            int userIndex = Convert.ToInt32(Console.ReadLine()); // storing user input as an integer
 
-            switch(userIndex)
+            while(userIndex < 0 || userIndex > 3) // continue looping while the user's input is less than 0 or larger than 3
             {
-                case 0:
-                    Console.WriteLine(myStrArray[0]);
-                    break;
-                case 1:
-                    Console.WriteLine(myStrArray[1]);
-                    break;
-                case 2:
-                    Console.WriteLine(myStrArray[2]);
-                    break;
-                case 3:
-                    Console.WriteLine(myStrArray[3]);
-                    break;
-                default:
-                    Console.WriteLine("Selected index doesn't exist.");
-                    break;
-
+                Console.WriteLine("Value not found. Please try again.");
+                Console.WriteLine("Please select a number between 0 and 3 to choose a color: "); // asking for user input
+                userIndex = Convert.ToInt32(Console.ReadLine()); 
             }
+            Console.WriteLine("You chose " + colors[userIndex] + ".\n");
+            Console.ReadLine();
 
             // Array of integers
             int[] myIntArray = { 12, 3, 45, 22, 8 };
-            Console.WriteLine("Please select an index of an array: ");
-            int userIndex1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please select an index between 0 and 4 to display a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
 
-            switch (userIndex1)
+            while(userNumber < 0 || userNumber > 4)
             {
-                case 0:
-                    Console.WriteLine(myIntArray[0]);
-                    break;
-                case 1:
-                    Console.WriteLine(myIntArray[1]);
-                    break;
-                case 2:
-                    Console.WriteLine(myIntArray[2]);
-                    break;
-                case 3:
-                    Console.WriteLine(myIntArray[3]);
-                    break;
-                case 4:
-                    Console.WriteLine(myIntArray[4]);
-                    break;
-                default:
-                    Console.WriteLine("Selected index doesn't exist.");
-                    break;
+                Console.WriteLine("Value not found. Please try again.");
+                Console.WriteLine("Please select an index between 0 and 4 to display a number: ");
+                userNumber = Convert.ToInt32(Console.ReadLine());
             }
+            Console.WriteLine("You chose " + myIntArray[userNumber] + ".\n");
+            Console.ReadLine();
+
 
             // List of strings
-            List<string> myStrList = new List<string>();
-            myStrList.Add("Hi");
-            myStrList.Add("how");
-            myStrList.Add("are");
-            myStrList.Add("you");
+            List<string> myStrList = new List<string> { "Austria", "Belgium", "Ireland", "Columbia", "Canada" }; //initiating a list of strings
+            myStrList.Add("Greece"); // adding a variable to the list of strings
 
-            Console.WriteLine("Please select an index of an array: ");
+            Console.WriteLine("Please select an index between 0 and 5 to display a country: ");
+            int userCountry = Convert.ToInt32(Console.ReadLine());
 
-            int userIndex2 = Convert.ToInt32(Console.ReadLine());
-
-            switch (userIndex2)
+            while(userCountry < 0 || userCountry > 5)
             {
-                case 0:
-                    Console.WriteLine(myStrList[0]);
-                    break;
-                case 1:
-                    Console.WriteLine(myStrList[1]);
-                    break;
-                case 2:
-                    Console.WriteLine(myStrList[2]);
-                    break;
-                case 3:
-                    Console.WriteLine(myStrList[3]);
-                    break;
-                default:
-                    Console.WriteLine("Selected index doesn't exist.");
-                    break;
+                Console.WriteLine("Value not found. Please try again.");
+                Console.WriteLine("Please select an index between 0 and 5 to display a country: ");
+                userCountry = Convert.ToInt32(Console.ReadLine());
             }
+            Console.WriteLine("You chose " + myStrList[userCountry] + ".");
             Console.ReadLine();
                 
-            
-
         }
     }
 }
